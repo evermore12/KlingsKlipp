91f524faf983665bc96d388529438099a56bf59f
jonathanbjornebro <jonathan.bjornebro@outlook.com>
2022-05-07 17:29:17 +0200

    Cleaned up some and fixed sliders

--- ClientApp/src/components/Day.js
+++ ClientApp/src/components/Day.js
@@ -1,6 +1,52 @@
 import '../css/Day.css'
+import { useEffect, useState } from 'react'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownToggle from 'react-bootstrap/esm/DropdownToggle'
+import '../css/Treatments.css'
 
 export default function Day() {
-    return (<></>)
+    const [treatments, setTreatments] = useState()
+    const [selectedTreatment, setSelectedTreatment] = useState()
+    const [loading, isLoading] = useState(true)
+
+    useEffect(() => {
+        populateDropdown();
+    }, [])
+
+    async function populateDays() {
+// {        let response = await fetch('treatments');
+//         let data = await response.json();
+//         setTreatments(data);
+//         isLoading(false);}
+    }
+    return (
+        <Dropdown id='dropdown'>
+                <DropdownToggle id='dropdown-toggle' variant='outline-success'>
+                    {selectedTreatment ?
+                    <>
+                                       <p className='icon'>{selectedTreatment.icon}</p>
+                                       <p className='name'>{selectedTreatment.name}</p>
+                                       <p className='price'>{selectedTreatment.price} kr</p>
+                                       </>
+                :
+                <p className='select-treatment'>
+                    Välj dag
+                </p>
+                }
+ 
+                </DropdownToggle>
+            <Dropdown.Menu id='dropdown-menu'>
+                {!loading &&
+                    treatments.map(treatment => {
+                        return (
+                            <>
+                                <Dropdown.Item key={treatment.treatmentId} id='dropdown-item' href="#/action-1" onClick={() => setSelectedTreatment(treatment)}>
+
+                                </Dropdown.Item>
+                            </>
+                        )
+                    })}
+            </Dropdown.Menu>
+        </Dropdown>)
 }
 
--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,13 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
