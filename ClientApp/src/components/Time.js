306f47b41124bcc34f6309be1066dae239170458
jonathanbjornebro <jonathan.bjornebro@outlook.com>
2022-05-08 03:15:22 +0200

    Added some syling to silders

--- ClientApp/src/components/Time.js
+++ ClientApp/src/components/Time.js
@@ -12,66 +12,64 @@ export default function Day() {
 
     useEffect(() => {
         noUiSlider.create(slider1.current, {
-            start: [40, 60, 80, 100],
-            behaviour: 'drag',
-            margin: 10,
-            limit: 30,
-            connect: [false, true, false, true, false],
-            range: {
-                'min': 20,
-                'max': 120
-            },
-        });
+            start: [5, 25, 30, 50, 75, 100],
+            connect:[false, true, false, true, false, true, false],
+            behaviour: "drag",
 
-        noUiSlider.create(slider2.current, {
-            start: 80,
+            // Disable animation on value-setting,
+            // so the sliders respond immediately.
             animate: false,
             range: {
-                min: 50,
+                min: 0,
                 max: 100
-            }
-        });
+            },
+            pips: {mode: 'count', values: 5}
+            
+        })
+        var connect = document.querySelectorAll('.noUi-connect');
+        var classes = ['c-1-color', 'c-2-color', 'c-3-color', 'c-4-color', 'c-5-color'];
+        
+        for (var i = 0; i < connect.length; i++) {
+            connect[i].classList.add(classes[i]);
+        }
 
-        slider1.current.noUiSlider.on('change', updateLockedValues);
-        slider2.current.noUiSlider.on('change', updateLockedValues);
+        // noUiSlider.create(slider2.current, {
+        //     start: 80,
+        //     animate: false,
+        //     range: {
+        //         min: 50,
+        //         max: 100
+        //     }
+        // });
 
-        slider1.current.noUiSlider.on('slide', function (values, handle) {
-            crossUpdate(values[handle], slider2.current);
-        });
+        // slider1.current.noUiSlider.on('slide', function (values, handle) {
+        //     crossUpdate(values[handle], slider2.current);
+        // });
 
-        slider2.current.noUiSlider.on('slide', function (values, handle) {
-            crossUpdate(values[handle], slider1.current);
-        });
+        // slider2.current.noUiSlider.on('slide', function (values, handle) {
+        //     crossUpdate(values[handle], slider1.current);
+        // });
     }, [])
 
-    function crossUpdate(value, slider) {
+    // function crossUpdate(value, slider) {
 
+    //     // Select whether to increase or decrease
+    //     // the other slider value.
+    //     var a = slider1.current === slider ? 0 : 1;
 
+    //     // Invert a
+    //     var b = a ? 0 : 1;
+    //     // Offset the slider value.
+    //     value -= lockedValues[b] - lockedValues[a];
 
-        // Select whether to increase or decrease
-        // the other slider value.
-        var a = slider1.current === slider ? 0 : 1;
+    //     // Set the value
+    //     slider.noUiSlider.set(value);
+    // }
 
-        // Invert a
-        var b = a ? 0 : 1;
-
-        // Offset the slider value.
-        value -= lockedValues[b] - lockedValues[a];
-
-        // Set the value
-        slider.noUiSlider.set(value);
-    }
-
-    function updateLockedValues() {
-        setLockedValues([
-            Number(slider1.current.noUiSlider.get()),
-            Number(slider2.current.noUiSlider.get())
-        ]);
-    }
     return (<>
         <div className='slider-container'>
             <div ref={slider1}></div>
-            <div ref={slider2}></div>
+            {/* <div ref={slider2}></div> */}
         </div>
         <section data-range-slider ng-model="range"></section>
     </>)
